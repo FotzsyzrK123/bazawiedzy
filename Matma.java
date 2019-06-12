b7585e35cff3c0202067b66c8dae6b5990d1de53
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-12 12:36:28 +0200

    lang math

---
+++ Matma.java
@@ -0,0 +1,14 @@
+import static java.lang.Math.*;
+
+class Matma{
+	public static void main(String[] args){
+	int num1 = 800;
+	double sqrt = sqrt(num1);
+	double wynik = pow(num1, 2);
+	double wynik1 = pow(num1, 3);
+	System.out.println("Pierwiastek liczby " + num1 + " wynosi: " + sqrt);
+	System.out.println("Kwadratem liczby " + num1 +  " jest: " + wynik);
+	System.out.println("Potega trzeciego stopnia liczby " + num1 + " jest: " + wynik1);
+	
+	}
+}
