15725912bdc6e59c526f011d55e24846de861dfb
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-18 20:44:34 +0200

    giereczka

--- Losowa.java
+++ Losowa.java
@@ -1,24 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Losowa {
 	public static void main(String[] args){
-	int liczba;
-	liczba = Integer.parseInt(args[0]);
-	int x;
-	x = 69;
-	if(liczba==x){		
-	System.out.println("Gratulacje! Zgadles! A szczesliwym numerkiem jest: " + x);
-	}
-	else if(liczba>(2*x)){	
-		System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za wysoka");
-	}
-	else if(liczba>x){
-		System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za wysoka");
-	}
-	else if(liczba<(x/2)){	
-		System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za mala");
-	}
-	else if(liczba<x){	
-		System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za mala");
-	}
+		Random random = new Random();
+		int x = random.nextInt(50); 
+		Scanner scan = new Scanner(System.in);
+		int liczba;
+		
+		
+			do{
+				System.out.println("Jaka liczbe wybralem?");
+				liczba = scan.nextInt();
+				if(liczba>(2*x)){	
+					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za wysoka");
+				}
+				else if(liczba>x){
+					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za wysoka");
+				}
+				else if(liczba<(x/2)){	
+					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za mala");
+				}
+				else if(liczba<x){	
+					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za mala");
+				}
+				else if(liczba==x){		
+				System.out.println("Gratulacje! Zgadles! A szczesliwym numerkiem jest: " + x);
+				}
+			} while(liczba!=x);
+		
 	
-}
+	}
 }
