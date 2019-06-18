eb408fc755713e1c32fc6fca9d0b6ba597f98a87
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-18 21:08:22 +0200

    gierczunia dodalem l prob

--- Losowa.java
+++ Losowa.java
@@ -6,28 +6,35 @@ public class Losowa {
 		int x = random.nextInt(50); 
 		Scanner scan = new Scanner(System.in);
 		int liczba;
+		int proby = 10;
 		
 		
 			do{
 				System.out.println("Jaka liczbe wybralem?");
 				liczba = scan.nextInt();
-				if(liczba>(2*x)){	
-					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za wysoka");
+				if(liczba==x){		
+					System.out.println("Gratulacje! Zgadles! A szczesliwym numerkiem jest: " + x);
 				}
-				else if(liczba>x){
-					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za wysoka");
-				}
-				else if(liczba<(x/2)){	
-					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za mala");
-				}
-				else if(liczba<x){	
-					System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za mala");
-				}
-				else if(liczba==x){		
-				System.out.println("Gratulacje! Zgadles! A szczesliwym numerkiem jest: " + x);
-				}
-			} while(liczba!=x);
-		
+				else{ 
+					if(liczba>(2*x)){	
+						System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za wysoka");
+					}
+					else if(liczba>x){
+						System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za wysoka");
+					}
+					else if(liczba<(x/2)){	
+						System.out.println("Podana liczba " + "(" + liczba + ")" + " jest ponad 2 razy za mala");
+					}
+					else if(liczba<x){	
+						System.out.println("Podana liczba " + "(" + liczba + ")" + " jest za mala");
+					}
+					proby = proby - 1;
+					System.out.println("Zostalo Ci prob " + proby);
+					}
+			}while(liczba!=x && proby > 0);
+		if(proby<=0){
+			System.out.println("Skonczyly Ci sie proby... leszczuuu");
+		}
 	
 	}
 }
