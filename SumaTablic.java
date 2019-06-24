349c7271c93ecb7563724fd4053d26961e80baa3
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-24 17:35:01 +0200

    Sumowanie tablic i inne pierdoły xD

---
+++ SumaTablic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+public class SumaTablic {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("No ile wierszy ma miec tablica? No ile? xD");
+	int x = scan.nextInt();
+	System.out.println("No jaka liczbe wybierasz? No jaka? xD");
+	float[] tablica = new float[x];
+	for(int i=0; i<tablica.length; i++){
+		tablica[i] = scan.nextFloat();
+	}
+	float suma = 0;
+	for(int i=0; i<tablica.length; i++){
+	suma = suma + tablica[i];
+	}
+	System.out.println(suma);
+}
+}
